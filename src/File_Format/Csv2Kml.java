c2ca778a73ce2b06fd0863a8c24966eaca9f8f42
OdeliaHochman <45036697+OdeliaHochman@users.noreply.github.com>
2018-12-04 12:56:33 +0200

    Update Csv2Kml.java
    updated Csv2Kml

--- src/File_Format/Csv2Kml.java
+++ src/File_Format/Csv2Kml.java
@@ -2,6 +2,7 @@ package File_Format;
 
 
 import java.io.BufferedReader;
+import GIS.Meta_data;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -20,14 +21,20 @@ public class Csv2Kml {
         ArrayList<String[]> element= new ArrayList<String[]>();
          
         String kmlstart = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "</kml xmlns=\"http://www.opengis.net/kml/2.2\">\n";
+                "<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n"+"<Document>"
+        		+"<Style id=\"red\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/red-dot.png</href></Icon></IconStyle></Style>"
+		 		+ "<Style id=\"yellow\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/yellow-dot.png</href></Icon></IconStyle></Style>"
+		 		+"<Style id=\"green\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/green-dot.png</href></Icon></IconStyle></Style>\r\n"  
+		 		+"<Folder>\n";
+        
         content.add(kmlstart);
 
-        String kmlend = "</kml>";
+        String kmlend ="</Document>\n"+"</kml>";
         try{
         	String kmlelement ="";
             FileWriter fw = new FileWriter(output);
             BufferedWriter bw = new BufferedWriter(fw);
+            String endFolder="</Folder>\n";
 
             for (int i = 0; i < a.size(); i++) 
             {
@@ -38,71 +45,27 @@ public class Csv2Kml {
             		String[] s = element.get(j);
             		
             		 kmlelement =
-                    		"\\\"</Document>" +
-                            "</Folder>"+
-                            "</name>"+s[1]+"</name>\n" +
-                    		"</Point>"+
-                            "</coordinates>"+s[0]+","+s[2]+","+s[4]+","+s[5]+","+s[6]+","+s[7]+","+s[8]+","+s[9]+","+s[10]+"</coordinates>" +
-                    		"</Point>"+
-                            "</Style id=\"red\">\n"+
-                    		"</TimeStamp>" + 
-                            "</when>\n" + s[3]+
-                    		"</TimeStamp>" + 
-                            "</href>http://maps.google.com/mapfiles/ms/icons/red-dot.png</href>\n"+
-                           "</IconStyle>"+
-                           "</description>"+s[6]+"</description>\n" +
-                            "</Point>\n" +
-                            "</Point>\n" +
-                            "</Style id=\"yellow\">n" +
-                    		"</TimeStamp>\n" + 
-                            "</when>\n" +
-                    		"</TimeStamp>" + 
-                    		 "</href>http://maps.google.com/mapfiles/ms/icons/yellow-dot.png</href>"+
-                    		 "</IconStyle>"+
-                    		 "</TimeStamp>" + 
-                    		 "</Point\n>"+
-                    		 "</Point\n>"+
-                     		"</TimeStamp>" +
-                     		"</when>\n" +
-                     		"</TimeStamp>" +
-                     		"</description>"+s[0]+","+s[2]+","+s[4]+","+s[5]+","+s[6]+","+s[7]+","+s[8]+","+s[9]+","+s[10]+"</description>\n" +
-                            "</Point>\n" +
-                            "</Point>\n" +
-                            "</Style id=\"green\">n"+
-                    		"</TimeStamp>\n" + 
-                            "</when>\n" +
-                    		"</TimeStamp>" + 
-                    		"</href>http://maps.google.com/mapfiles/ms/icons/green-dot.png</href>\\n"+
-                    		 "</IconStyle>"+
-                    		 "</TimeStamp>" + 
-                    		 "</Point\n>"+
-                    		 "</Point\n>"+
-                     		"</TimeStamp>" + 
-                     		"</when>\n" + s[3]+
-                     		"</TimeStamp>" +
-                     		 "</href></Icon></IconStyle></Style><Folder><name>Wifi Networks</name> "+
-                            "</Placemark>\n";
-            		
-            		String nameStart = "<Placemark><name><![CDATA]";
+            		 		"<name>Wifi Networks</name>\n"
+            		 		+"<Placemark>\n"
+            		 		+"<name>"+s[1]+"</name>\n" 
+                    		+"<description>"+s[0]+","+s[2]+","+s[4]+","+s[5]+","+s[8] +s[9]+","+s[10]+"</description>\n"
+                    		+"<styleUrl>#red</styleUrl>"
+                    		+"<Point>\n"
+                    		+"<coordinates>"+s[6]+s[7]+"</coordinates>\n"
+                    		+"</Point>\n"
+                    		//+"<TimeStamp>"+s[3]+"</TimeStamp>\n" 
+                    		+"<TimeStamp>\n"+
+    						"<when>"+s[3]+"</when>\n"+
+    						"</TimeStamp>\n"
+                    		+"</Placemark>\n";
 
-            		String descriptionStart = "]]></name><description><![CDATA]";
+                     content.add(kmlelement);
 
-            		String frequencyStart= "</b><br/>Frequency: <b>";
+				}      
 
-            		String date = "</b><br/>Date: <b>";
-
-            		String end = "</b>]]></description><styleUrl>#";
-
-            		String pointStart = "</styleUrl><Point><coordinates>";
-
-            		String pointEnd = "</coordinates></Point></Placemark>";
-
-            		kmlelement += "</Folder></Document></kml>";
-
-				}
-            	   
             }
-            content.add(kmlelement);
+            content.add(endFolder);
+
             content.add(kmlend);
             String csv = content.toString().replaceAll(",", "").replace("[", "").replace("]", "");
             bw.write(csv);
@@ -114,3 +77,4 @@ public class Csv2Kml {
     }
 
 }
+
